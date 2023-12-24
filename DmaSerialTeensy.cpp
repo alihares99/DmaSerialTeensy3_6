0540eb19ab838dc73619f271c4f621699e80c0b9
Hares <46823801+alihares99@users.noreply.github.com>
2023-12-24 13:25:04 +0800

    Refactoring

--- DmaSerialTeensy.cpp
+++ DmaSerialTeensy.cpp
@@ -6,9 +6,8 @@
 #include "DmaSerialTeensy.h"
 #include "Arduino.h"
 #include <cstring>
+#include <algorithm>
 
-#define MIN(x, y)  ((x) > (y) ? (y) : (x))
-#define MAX(x, y)  ((x) > (y) ? (x) : (y))
 
 #ifdef __MK66FX1M0__ // teensy 3.6
 const DmaSerialTeensy::SerialPin DmaSerialTeensy::serialPins[6][3] = {
@@ -28,14 +27,9 @@ DmaSerialTeensy dmaSerial4(4);
 #endif
 
 void DmaSerialTeensy::txCompleteCallback1() {dmaSerial1.txIsr();}
-void DmaSerialTeensy::rxCompleteCallback1() {dmaSerial1.rxIsr();}
 void DmaSerialTeensy::txCompleteCallback2() {dmaSerial2.txIsr();}
-void DmaSerialTeensy::rxCompleteCallback2() {dmaSerial2.rxIsr();}
 void DmaSerialTeensy::txCompleteCallback3() {dmaSerial3.txIsr();}
-void DmaSerialTeensy::rxCompleteCallback3() {dmaSerial3.rxIsr();}
 void DmaSerialTeensy::txCompleteCallback4() {dmaSerial4.txIsr();}
-void DmaSerialTeensy::rxCompleteCallback4() {dmaSerial4.rxIsr();}
-void DmaSerialTeensy::rxCompleteCallback5() {/*dmaSerial5.rxIsr(); // not supported yet*/}
 
 DmaSerialTeensy::DmaSerialTeensy(int serialNo)
     : serialNo(serialNo)
@@ -77,7 +71,7 @@ void DmaSerialTeensy::begin(uint32_t baud) {
 	}
 
     if (!dmaChannelSend) {
-        void (*txIsr)();
+        void (*txIsr)() = nullptr;
         uint8_t dmamuxSource;
         switch (serialNo) {
             case 1:
@@ -116,29 +110,24 @@ void DmaSerialTeensy::begin(uint32_t baud) {
             // not enabled here
         }
     }
+    
     if (!dmaChannelReceive) {
-        void (*rxIsr)();
         uint8_t dmamuxSource;
         switch (serialNo) {
             case 1:
                 dmamuxSource = DMAMUX_SOURCE_UART0_RX;
-                rxIsr = rxCompleteCallback1;
                 break;
             case 2:
                 dmamuxSource = DMAMUX_SOURCE_UART1_RX;
-                rxIsr = rxCompleteCallback2;
                 break;
             case 3:
                 dmamuxSource = DMAMUX_SOURCE_UART2_RX;
-                rxIsr = rxCompleteCallback3;
                 break;
             case 4:
                 dmamuxSource = DMAMUX_SOURCE_UART3_RX;
-                rxIsr = rxCompleteCallback4;
                 break;
             case 5:
                 dmamuxSource = DMAMUX_SOURCE_UART4_RXTX;
-                rxIsr = rxCompleteCallback5;
                 break;
             default:
                 return;
@@ -147,13 +136,10 @@ void DmaSerialTeensy::begin(uint32_t baud) {
         dmaChannelReceive->source(uartBaseAddr->D);
         dmaChannelReceive->destinationBuffer(rxBuffer, DMA_RX_BUFFER_SIZE);
         dmaChannelReceive->triggerAtHardwareEvent(dmamuxSource);
-        // no need for rx Interrupt:
-        // dmaChannelReceive->attachInterrupt(rxIsr);
-        // dmaChannelReceive->interruptAtCompletion();
         dmaChannelReceive->enable();
     }
 
-    uint32_t divisor;
+    uint32_t divisor = 1;
     switch (serialNo) {
         case 1:
             SIM_SCGC4 |= SIM_SCGC4_UART0;	// turn on clock
@@ -196,7 +182,7 @@ void DmaSerialTeensy::begin(uint32_t baud) {
         #endif
         case 9: CORE_PIN9_CONFIG = PORT_PCR_PE | PORT_PCR_PS | PORT_PCR_PFE | PORT_PCR_MUX(3); break;
         #if defined(__MK20DX128__) || defined(__MK20DX256__)    // T3.0, T3.1, T3.2
-            case 26: CORE_PIN26_CONFIG = PORT_PCR_PE | PORT_PCR_PS | PORT_PCR_PFE | PORT_PCR_MUX(3); break;
+        case 26: CORE_PIN26_CONFIG = PORT_PCR_PE | PORT_PCR_PS | PORT_PCR_PFE | PORT_PCR_MUX(3); break;
         #elif defined(__MK64FX512__) || defined(__MK66FX1M0__)  // T3.5 or T3.6
         case 59: CORE_PIN59_CONFIG = PORT_PCR_PE | PORT_PCR_PS | PORT_PCR_PFE | PORT_PCR_MUX(3); break;
         #endif
@@ -217,7 +203,7 @@ void DmaSerialTeensy::begin(uint32_t baud) {
         #endif
         case 10: CORE_PIN10_CONFIG = PORT_PCR_DSE | PORT_PCR_SRE | PORT_PCR_MUX(3); break;
         #if defined(__MK20DX128__) || defined(__MK20DX256__)    // T3.0, T3.1, T3.2
-            case 31: CORE_PIN31_CONFIG = PORT_PCR_DSE | PORT_PCR_SRE | PORT_PCR_MUX(3); break;
+        case 31: CORE_PIN31_CONFIG = PORT_PCR_DSE | PORT_PCR_SRE | PORT_PCR_MUX(3); break;
         #elif defined(__MK64FX512__) || defined(__MK66FX1M0__)  // T3.5 or T3.6
         case 58: CORE_PIN58_CONFIG = PORT_PCR_DSE | PORT_PCR_SRE | PORT_PCR_MUX(3); break;
         #endif
@@ -242,25 +228,22 @@ void DmaSerialTeensy::begin(uint32_t baud) {
     uartBaseAddr->C5 |= UART_C5_TDMAS;
     uartBaseAddr->C5 |= UART_C5_RDMAS;
     uartBaseAddr->PFIFO = 0;
-    /*UART0_TWFIFO = 2; // tx watermark, causes S1_TDRE to set
-    UART0_RWFIFO = 4; // rx watermark, causes S1_RDRF to set
-    UART0_PFIFO = UART_PFIFO_TXFE | UART_PFIFO_RXFE;*/
 
     // enable transmitter and receiver
     uartBaseAddr->C2 |= (UART_C2_TE | UART_C2_RE);
 }
 
 int DmaSerialTeensy::available() {
-    int biter = dmaChannelReceive->TCD->BITER;
-    int citer = dmaChannelReceive->TCD->CITER;
-    int csr = dmaChannelReceive->TCD->CSR;
+    size_t biter = dmaChannelReceive->TCD->BITER;
+    size_t citer = dmaChannelReceive->TCD->CITER;
+    auto csr = dmaChannelReceive->TCD->CSR;
     if (csr & 0x80) { // done so Rx buffer is full
         if (rxBufferTail == 0) return 0;
         else return DMA_RX_BUFFER_SIZE - rxBufferTail;
     }
     else {
         // our version of buffer indexes are not update
-        int head = biter - citer;
+        auto head = biter - citer;
         if (head >= rxBufferTail) return head - rxBufferTail;
         else return head - rxBufferTail + DMA_RX_BUFFER_SIZE;
     }
@@ -288,18 +271,18 @@ size_t DmaSerialTeensy::write(char c) {
 
 size_t DmaSerialTeensy::write(const uint8_t *p, size_t len) {
 
-    int index = 0;
+    size_t index = 0;
     while (index < len) {
 
         // wait until there is free space in the buffer:
         while (DMA_TX_BUFFER_SIZE - txBufferCount /* free size */ == 0);
 
         // get a chunk of data to add to the buffer
-        int chunkSize = MIN(len - index, DMA_TX_BUFFER_SIZE - txBufferCount);
+        size_t chunkSize = std::min(len - index, DMA_TX_BUFFER_SIZE - txBufferCount);
 
         // copy the data to the buffer:
-        int s1 = MIN(chunkSize, DMA_TX_BUFFER_SIZE - txBufferHead);
-        int s2 = chunkSize - s1;
+        size_t s1 = std::min(chunkSize, DMA_TX_BUFFER_SIZE - txBufferHead);
+        size_t s2 = chunkSize - s1;
         memcpy(&txBuffer[txBufferHead], &p[index], s1);
         if (s2 > 0)
             memcpy(&txBuffer[0], &p[index + s1], s2);
@@ -315,8 +298,8 @@ size_t DmaSerialTeensy::write(const uint8_t *p, size_t len) {
         if (!transmitting) {
             transmitting = true;
             __disable_irq()
-            int count = MIN(DMA_TX_BUFFER_SIZE - txBufferTail, chunkSize);
-            count = MIN(count, DMA_MAX_BURST_DATA_TRANSFER); // MIN(remaining in the buffer, len_truncate, max_burst)
+            size_t count = std::min(DMA_TX_BUFFER_SIZE - txBufferTail, chunkSize);
+            count = std::min(count, size_t(DMA_MAX_BURST_DATA_TRANSFER)); // MIN(remaining in the buffer, len_truncate, max_burst)
             dmaChannelSend->sourceBuffer(&txBuffer[txBufferTail], count);
             dmaChannelSend->enable();
             __enable_irq();
@@ -324,19 +307,6 @@ size_t DmaSerialTeensy::write(const uint8_t *p, size_t len) {
     }
     return len;
 
-}
-
-void DmaSerialTeensy::rxIsr() {
-    dmaChannelReceive->clearInterrupt();
-
-    // no need for rx interrupt
-    /*
-    // move the head:
-    int count = dmaChannelReceive->TCD->BITER;
-    rxBufferHead += count;
-    if (rxBufferHead >= DMA_RX_BUFFER_SIZE)
-        rxBufferHead -= DMA_RX_BUFFER_SIZE;
-    */
 }
 
 void DmaSerialTeensy::txIsr() {
@@ -355,8 +325,8 @@ void DmaSerialTeensy::txIsr() {
     if (txBufferCount > 0) {
         transmitting = true;
         __disable_irq()
-        int count = MIN(DMA_TX_BUFFER_SIZE - txBufferTail, txBufferCount);
-        count = MIN(count, DMA_MAX_BURST_DATA_TRANSFER); // MIN(remaining in the buffer, txBufferCount, max_burst)
+        size_t count = std::min(DMA_TX_BUFFER_SIZE - txBufferTail, size_t(txBufferCount));
+        count = std::min(count, size_t(DMA_MAX_BURST_DATA_TRANSFER)); // MIN(remaining in the buffer, txBufferCount, max_burst)
         dmaChannelSend->sourceBuffer(&txBuffer[txBufferTail], count);
         dmaChannelSend->enable();
         __enable_irq();
--- DmaSerialTeensy.h
+++ DmaSerialTeensy.h
@@ -3,8 +3,7 @@
  * You are free to use this file in any project as long as you keep my email address alihares99@gmail.com here.
  */
 
-#ifndef DMA_FOR_TEENSY_DMASERIALTEENSY_H
-#define DMA_FOR_TEENSY_DMASERIALTEENSY_H
+#pragma once
 
 #include "kinetis.h"
 #include "core_pins.h"
@@ -19,49 +18,9 @@
 
 #define DMA_MAX_BURST_DATA_TRANSFER 511         // This is the maximum data we are putting into DMA at once
 
-class DmaSerialTeensy : public Stream{
-
-private:
-    int serialNo;
-    int rxPinNo;
-    int txPinNo;
-
-    uint8_t* txBuffer = nullptr;
-    uint8_t* rxBuffer = nullptr;
-    volatile uint32_t txBufferTail;
-    volatile uint32_t txBufferHead;
-    volatile uint32_t txBufferCount;
-    volatile uint32_t rxBufferTail;
-    //volatile uint32_t rxBufferHead; // no need for this
-
-    volatile bool transmitting = false;
-
-    KINETISK_UART_t* uartBaseAddr = nullptr;
-    DMAChannel* dmaChannelSend = nullptr;
-    DMAChannel* dmaChannelReceive = nullptr;
-
-    typedef struct {
-        int serialPin_rx;
-        int serialPin_tx;
-    } SerialPin;
-
-    static const SerialPin serialPins[6][3];
-
-    static void txCompleteCallback1();
-    static void rxCompleteCallback1();
-    static void txCompleteCallback2();
-    static void rxCompleteCallback2();
-    static void txCompleteCallback3();
-    static void rxCompleteCallback3();
-    static void txCompleteCallback4();
-    static void rxCompleteCallback4();
-    static void rxCompleteCallback5();
-
-    void rxIsr();
-    void txIsr();
-
+class DmaSerialTeensy : public Stream
+{
 public:
-
     explicit DmaSerialTeensy(int serialNo);
     int peek() override;
     void begin(uint32_t baud);
@@ -75,6 +34,38 @@ public:
     size_t write(long n)            { return write((uint8_t)n); }
     size_t write(unsigned int n)    { return write((uint8_t)n); }
     size_t write(int n)             { return write((uint8_t)n); }
+
+private:
+    int serialNo;
+    int rxPinNo;
+    int txPinNo;
+
+    uint8_t* txBuffer = nullptr;
+    uint8_t* rxBuffer = nullptr;
+    volatile size_t txBufferTail;
+    volatile size_t txBufferHead;
+    volatile size_t txBufferCount;
+    volatile size_t rxBufferTail;
+
+    volatile bool transmitting = false;
+
+    KINETISK_UART_t* uartBaseAddr = nullptr;
+    DMAChannel* dmaChannelSend = nullptr;
+    DMAChannel* dmaChannelReceive = nullptr;
+
+    struct SerialPin{
+        int serialPin_rx;
+        int serialPin_tx;
+    };
+
+    static const SerialPin serialPins[6][3];
+
+    static void txCompleteCallback1();
+    static void txCompleteCallback2();
+    static void txCompleteCallback3();
+    static void txCompleteCallback4();
+
+    void txIsr();
 };
 
 extern DmaSerialTeensy dmaSerial1;
@@ -82,4 +73,3 @@ extern DmaSerialTeensy dmaSerial2;
 extern DmaSerialTeensy dmaSerial3;
 extern DmaSerialTeensy dmaSerial4;
 
-#endif //DMA_FOR_TEENSY_DMASERIALTEENSY_H
